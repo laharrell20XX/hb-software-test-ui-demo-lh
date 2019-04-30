4423a9467b4cc0684208cdd6fcb77c3d7d68ec21
laharrell20XX <lharrell@basecampcodingacademy.org>
2019-04-30 10:37:50 -0500

    Updates

---
+++ precache-manifest.b35dda7d5c5bb6aa9142092972e9dbbb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d0d9ba0939445ef9ff7703195505f88f",
+    "url": "/hb-software-test-ui-demo-lh/index.html"
+  },
+  {
+    "revision": "9e83775b565d82677dc2",
+    "url": "/hb-software-test-ui-demo-lh/static/css/main.5f1639f8.chunk.css"
+  },
+  {
+    "revision": "4d1690f8aeb8fd0ecb8e",
+    "url": "/hb-software-test-ui-demo-lh/static/js/2.63a889e5.chunk.js"
+  },
+  {
+    "revision": "9e83775b565d82677dc2",
+    "url": "/hb-software-test-ui-demo-lh/static/js/main.e99a1c83.chunk.js"
+  },
+  {
+    "revision": "a4f54cd245356baf343f",
+    "url": "/hb-software-test-ui-demo-lh/static/js/runtime~main.ebe8298d.js"
+  }
+]);
