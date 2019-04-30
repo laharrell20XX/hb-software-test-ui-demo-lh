fffb9d7fb8a492b51afc76fa0d082f27ba101944
laharrell20XX <lharrell@basecampcodingacademy.org>
2019-04-30 00:14:29 -0500

    replaced InvoiceList with routing switch
    - Switch component (8 - 11)
      - inside are two Route components:
        1. for root path where it renders the InvoiceList component
        (9)
        2. for /invoices path which also renders InvoiceList comp
        (10)
      - 2nd route is the intended path
      - 1st route is an alias

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,10 +1,15 @@
 import React from 'react';
+import { Switch, Route } from 'react-router-dom';
 import './App.css';
 import InvoiceList from '../InvoiceList';
 
 function App() {
   return (
-    <InvoiceList />
+    <Switch>
+      <Route exact path="/" component={InvoiceList}></Route>
+      <Route exact path="/invoices" component={InvoiceList}></Route>
+    </Switch>
+
   );
 }
 
