7b92b954d0faa0994ffc5fdcabb5d34a9ca390eb
laharrell20XX <lharrell@basecampcodingacademy.org>
2019-04-30 00:38:44 -0500

    added route to invoice details component
    - path of route is "/invoices/:id" where id is a url param
    - id will refer to specific invoice id
    - component to render is LineItemList

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 import './App.css';
 import InvoiceList from '../InvoiceList';
+import LineItemList from '../LineItemList';
 
 function App() {
   return (
     <Switch>
       <Route exact path="/" component={InvoiceList}></Route>
       <Route exact path="/invoices" component={InvoiceList}></Route>
+      <Route path="/invoices/:id" component={LineItemList}></Route>
     </Switch>
 
   );
