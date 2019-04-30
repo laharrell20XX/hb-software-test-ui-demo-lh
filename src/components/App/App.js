5768551dbe8f4a40052c5605c2244ce4d873b2bb
laharrell20XX <lharrell@basecampcodingacademy.org>
2019-04-29 20:59:40 -0500

    moved html into InvoiceList component
    - renders InvoiceList component instead of explicit
    HTML

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,38 +1,10 @@
 import React from 'react';
 import './App.css';
+import InvoiceList from '../InvoiceList';
 
 function App() {
   return (
-    <div className="invoice-list">
-      <button>New Invoice</button>
-      <div>
-        <table>
-          <thead className="invoice-table-header">
-            <tr>
-              <th colSpan = "1">
-                Invoice #
-              </th>
-              <th colSpan = "1">
-                Total
-              </th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr className="invoice-table-row">
-              <td>
-                0001
-              </td>
-              <td>
-                $0.00
-              </td>
-              <td>
-              <button>Details</button>
-              </td>
-            </tr>
-          </tbody>
-        </table>
-      </div>
-    </div>
+    <InvoiceList />
   );
 }
 
